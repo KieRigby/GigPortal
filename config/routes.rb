152c1a52c55c6f2b4d03ec2c38d79096d4f980bd
Kieran Rigby <kieran@kierigby.com>
2018-11-27 21:54:49 +0000

    Added Home Controller and adjusted routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  #Root view to the home controller home action
+  root 'home#home'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
