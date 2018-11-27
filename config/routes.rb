b0a71708c8187ae4d86782564c3ff5ac5f9cf78a
Kieran Rigby <kieran@kierigby.com>
2018-11-27 22:00:25 +0000

    Added contact action, view and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   #Root view to the home controller home action
   root 'home#home'
+  #Contact GET route
+  get 'contact', to: 'home#contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
