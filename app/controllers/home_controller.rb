479c99c6433dafeb34fb00a345b281967df421f7
Kieran Rigby <kieran@kierigby.com>
2018-11-29 21:38:37 +0000

    Playing with design of home page... in the wrong branch... sorry

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,5 +6,17 @@ class HomeController < ApplicationController
   end
 
   def request_contact
+    name = params[:name]
+    email = params[:email]
+    telephone = params[:telephone]
+    message = params[:message]
+
+    if email.blank?
+      flash[:alert] = L18N.t('home.contact.no-email')
+    else
+      flash[:notice] = L18N.t('home.contact.email-sent')
+    end
+
+    redirect_to contact_path
   end
 end
--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -2,8 +2,16 @@ require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get home_home_url
+    get root_path
     assert_response :success
+  end
+
+  test "should get contact" do
+    get contact_path
+    assert_response :success
+    assert_template layout: 'application'
+    assert_select 'title', 'Contact Us'
+    assert_select 'description', 'If you have any problems or even just some general feedback, why not send us a message using the contact form below?'
   end
 
 end
