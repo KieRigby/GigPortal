69f344ae07cdef2ae134d3a75550a8ab8a07fa0a
Kieran Rigby <kieran@kierigby.com>
2018-12-06 05:05:40 +0000

    Added published scope to events index

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,8 @@
 class Event < ApplicationRecord
+  validates :title, presence: true
+  validates :short_description, presence: true
+  validates :long_description, presence: true
+  validates :published, presence: true
+
+  scope :published, -> {where(published:true)}
 end
