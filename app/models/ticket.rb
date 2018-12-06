dd5d13626501de443e8a14f6c073ac1dfcfe7c71
Kieran Rigby <kieran@kierigby.com>
2018-12-06 11:35:46 +0000

    Added ticket validations

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,3 +1,8 @@
 class Ticket < ApplicationRecord
+  validates :ticket_hash, presence: true
+  validates :ticket_hash, unique: true
+  validates :event, presence: true
+  validates :scanned, presence: true
+  
   belongs_to :event
 end
