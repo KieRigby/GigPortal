8e40a8e4d5c75ffac2b94665c912ea01aa9aa978
Kieran Rigby <kieran@kierigby.com>
2018-12-07 04:49:58 +0000

    Started scopes

--- app/controllers/ticket_controller.rb
+++ app/controllers/ticket_controller.rb
@@ -3,7 +3,6 @@ require 'rqrcode'
 class TicketController < ApplicationController
   before_action :set_ticket, only: [:view]
 
-
   def view
     @qr = RQRCode::QRCode.new(@ticket.ticket_hash, :size => 5, :level => :h)
   end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,7 +2,6 @@ class Ticket < ApplicationRecord
   validates :ticket_hash, presence: true
   validates :ticket_hash, uniqueness: true
   validates :event, presence: true
-  validates :scanned, presence: true
 
   belongs_to :event
 
